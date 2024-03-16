174c6c13ace3ee1b2d549bc4f6665d43fbf36518
C-W-Z <chenweizhang3021@gmail.com>
2024-03-16 12:02:16 +0800

    + construct radiance map

--- hdr.py
+++ hdr.py
@@ -2,9 +2,9 @@ import os
 import cv2
 import numpy as np
 
-def gsolve(Z:np.ndarray[int, 2], B:np.ndarray[float], l:float, w:np.ndarray[float]) -> tuple[np.ndarray[float], np.ndarray[float]]:
+def solve_response_function(Z:np.ndarray[int, 2], lnt:np.ndarray[float], l:float, w:np.ndarray[float]) -> tuple[np.ndarray[float], np.ndarray[float]]:
     """
-    Solve for imaging system response function
+    Solve for imaging system response function (camera response function, CRF)
 
     Solve the least square solution of Debevec matrix equation Ax=b
     A : [N * P + 1 + 254] * [256 + N]
@@ -12,18 +12,18 @@ def gsolve(Z:np.ndarray[int, 2], B:np.ndarray[float], l:float, w:np.ndarray[floa
     b : [N * P + 1 + 254] * 1
 
     Parameters:
-    Z[i,j]: The pixel value of pixel location i in image j (Z.size=N*P, Z.min=0, Z.max=255)
-    B[j]  : The log delta t or log shutter speed for image j (B.len=P)
-    l     : lambda, the constant that determines the amount of smoothness
-    w[z]  : weighting function value for pixel value z (w.len=256)
+    Z[i,j] : The pixel value of pixel location i in image j (Z.size=N*P, Z.min=0, Z.max=255)
+    lnt[j] : The log delta t or log shutter speed for image j (B.len=P)
+    l      : lambda, the constant that determines the amount of smoothness
+    w[z]   : weighting function value for pixel value z (w.len=256)
 
     Returns:
-    g[z]  : log exposure corresponding to pixel value z (g.len=256)
-    lE[i] : log film irradiance at pixel location i (len=N)
+    g[z]   : log exposure corresponding to pixel value z (g.len=256)
+    lnE[i] : log film irradiance at pixel location i (len=N)
     """
 
     N, P = Z.shape
-    assert(len(B) == P and len(w) == 256)
+    assert(len(lnt) == P and len(w) == 256)
     A = np.zeros((N * P + 1 + 254, 256 + N))
     b = np.zeros((N * P + 1 + 254, 1))
     k = 0
@@ -33,7 +33,7 @@ def gsolve(Z:np.ndarray[int, 2], B:np.ndarray[float], l:float, w:np.ndarray[floa
             wij = w[zij]
             A[k, zij] = wij
             A[k, 256 + i] = -wij
-            b[k, 0] = wij * B[j]
+            b[k, 0] = wij * lnt[j]
             k += 1
     # Fix the curve by setting its middle value to 0
     A[k, 128] = 1
@@ -46,3 +46,33 @@ def gsolve(Z:np.ndarray[int, 2], B:np.ndarray[float], l:float, w:np.ndarray[floa
     x, _, _, _ = np.linalg.lstsq(A, b, rcond = None)
     x = x.ravel()
     return (x[:256], x[256:])
+
+def construct_radiance_map(Z:np.ndarray[int, 2], g:np.ndarray[float], lnt:np.ndarray[float], w:np.ndarray[float]) -> np.ndarray[float]:
+    """
+    Construct the radiance map (size is same as images)
+
+    Parameters:
+    Z[i,j] : The pixel value of pixel location i in image j (Z.size=N*P, Z.min=0, Z.max=255)
+    g[z]   : log exposure corresponding to pixel value z (g.len=256)
+    lnt[j] : The log delta t or log shutter speed for image j (B.len=P)
+    w[z]   : weighting function value for pixel value z (w.len=256)
+
+    Returns:
+    lnE[z] : log film irradiance at pixel location i (len=N)
+    """
+
+    N, P = Z.shape
+    assert(len(g) == 256 and len(lnt) == P and len(w) == 256)
+    lnE = np.zeros((N))
+    for i, pixel in enumerate(Z):
+        wgtSum = 0
+        wSum = 0
+        for j, zij in enumerate(pixel):
+            wgtSum += w[zij] * (g[zij] - lnt[j])
+            wSum += w[zij]
+        if wSum > 0:
+            lnE[i] = wgtSum / wSum
+        else:
+            lnE[i] = wgtSum
+
+    return lnE
