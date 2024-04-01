25e01be672ee3eb4e4ccec7139f00d63944a83bc
C-W-Z <chenweizhang3021@gmail.com>
2024-04-01 08:37:20 +0800

    adjust folder structure

---
+++ code/tonemap.py
@@ -0,0 +1,224 @@
+import argparse
+import math
+import os
+import cv2
+import numpy as np
+from enum import IntEnum
+import utils
+
+class NormalizeType(IntEnum):
+    NONE = 0
+    ALL = 1
+    CHANNEL = 2
+    def __str__(self):
+        return self.name.upper()
+
+def to_gray(hdr:np.ndarray):
+    if hdr.ndim == 3:
+        # channel order is BGR
+        # add 1e-6 to avoid zero
+        return cv2.cvtColor(hdr, cv2.COLOR_BGR2GRAY) + 1e-6
+        # return 0.27 * hdr[:,:,2] + 0.67 * hdr[:,:,1] + 0.06 * hdr[:,:,0] + 1e-6
+        # return 0.299 * hdr[:,:,2] + 0.587 * hdr[:,:,1] + 0.114 * hdr[:,:,0] + 1e-6
+    return hdr
+
+def cv2_drago(hdr:np.ndarray[np.float32, 3], output_dir:str, gamma:float, saturation:float, bias:float, brightness:float):
+
+    print(f"cv2 Drago tonemapping ...\ngamma = {gamma}\nsaturation = {saturation}\nbias = {bias}\nbrightness = {brightness}")
+
+    tonemap = cv2.createTonemapDrago(gamma, saturation, bias)
+    ldr = tonemap.process(hdr)
+    ldr *= brightness * 255
+    filename = f"cv2_drago_{gamma}_{saturation}_{bias}_{brightness}.png"
+    filename = os.path.join(output_dir, filename)
+    print(f"saving LDR image to {filename}")
+    cv2.imwrite(filename, ldr)
+
+def single_global(Lworld:np.ndarray[np.float32, 2], a:float, Lwhite:float, delta:float=1e-6) -> np.ndarray[np.float32, 2]:
+    """
+    Photographic global mapping for single channel
+    """
+
+    H, W = Lworld.shape
+    N = H * W
+    l_min = np.min(Lworld)
+    # print(f"l_min={l_min}")
+    if l_min < 0:
+        delta += np.abs(l_min)
+    average_Lworld = np.exp(np.sum(np.log(delta + Lworld)) / N)
+    Lm = a * Lworld / average_Lworld
+    Ld = Lm * (1 + Lm / np.square(Lwhite)) / (1 + Lm)
+    return Ld
+
+def gamma_intensity(hdr:np.ndarray[np.float32, 3], output_dir:str, gamma:float, brightness:float, normalize:NormalizeType=NormalizeType.ALL):
+    print(f"gamma intensity tonemapping ...\ngamma = {gamma}\nbrightness = {brightness}\nnormalize = {normalize}")
+
+    Lworld = to_gray(hdr)
+    # print(np.max(Lworld), np.mean(Lworld), np.min(Lworld))
+    Ld = np.power(Lworld, 1/gamma)
+
+    ldr = np.zeros_like(hdr)
+    for i in range(3):
+        ldr[:,:,i] = hdr[:,:,i] * Ld / Lworld
+        if normalize == NormalizeType.CHANNEL:
+            ldr[:,:,i] = cv2.normalize(ldr[:,:,i], None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    if normalize == NormalizeType.ALL:
+        ldr = cv2.normalize(ldr, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    ldr *= brightness * 255
+
+    filename = f"gamma_intensity_{gamma}_{brightness}_{normalize}.png"
+    filename = os.path.join(output_dir, filename)
+    print(f"saving LDR image to {filename}")
+    cv2.imwrite(filename, ldr)
+
+    return ldr
+
+def gamma_color(hdr:np.ndarray[np.float32, 3], output_dir:str, gamma:float, brightness:float, normalize:NormalizeType=NormalizeType.ALL):
+    print(f"gamma color tonemapping ...\ngamma = {gamma}\nbrightness = {brightness}\nnormalize = {normalize}")
+
+    Lworld = to_gray(hdr)
+    # print(np.max(Lworld), np.mean(Lworld), np.min(Lworld))
+    L = Lworld / (1 / brightness + Lworld)
+
+    ldr = np.zeros_like(hdr)
+    for i in range(3):
+        ldr[:,:,i] = np.power(hdr[:,:,i] / Lworld, 1/gamma) * L
+        if normalize == NormalizeType.CHANNEL:
+            ldr[:,:,i] = cv2.normalize(ldr[:,:,i], None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    if normalize == NormalizeType.ALL:
+        ldr = cv2.normalize(ldr, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    ldr *= brightness * 255
+
+    filename = f"gamma_color_{gamma}_{brightness}_{normalize}.png"
+    filename = os.path.join(output_dir, filename)
+    print(f"saving LDR image to {filename}")
+    cv2.imwrite(filename, ldr)
+
+    return ldr
+
+def photographic_global(hdr:np.ndarray[np.float32, 3], output_dir:str, a:float, Lwhite:float, delta:float, normalize:NormalizeType=NormalizeType.ALL, save_gray:bool=False):
+    print(f"phtographic global tonemapping ...\na = {a}\nLwhite = {Lwhite}\ndelta = {delta}\nnormalize = {normalize}")
+
+    Lworld = to_gray(hdr)
+    Ld = single_global(Lworld, a, Lwhite, delta)
+
+    if save_gray:
+        filename = f"global_gray_{a}_{Lwhite}_{delta}_{normalize}.png"
+        filename = os.path.join(output_dir, filename)
+        print(f"saving LDR gray image to {filename}")
+        cv2.imwrite(filename, Ld * 255)
+
+    ldr = np.zeros_like(hdr, dtype=np.uint8)
+    for i in range(3):
+        ldr[:,:,i] = single_global(hdr[:,:,i] * Ld / Lworld, a, Lwhite, delta) * 255
+        if normalize == NormalizeType.CHANNEL:
+            ldr[:,:,i] = cv2.normalize(ldr[:,:,i], None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    if normalize == NormalizeType.ALL:
+        ldr = cv2.normalize(ldr, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    filename = f"global_{a}_{Lwhite}_{delta}_{normalize}.png"
+    filename = os.path.join(output_dir, filename)
+    print(f"saving LDR image to {filename}")
+    cv2.imwrite(filename, ldr)
+
+    return ldr
+
+def bilateral_filtering(hdr:np.ndarray[np.float32, 3], output_dir:str, sigma_range:float, contrast:float, a:float, Lwhite:float, delta:float, normalize:NormalizeType=NormalizeType.ALL, save_filtered:bool=False):
+    print(f"bilateral filtering tonemapping ...\nsigma_range = {sigma_range}\ncontrast = {contrast}\na = {a}\nLwhite = {Lwhite}\ndelta = {delta}\nnormalize = {normalize}")
+
+    Lworld = to_gray(hdr)
+    H, W = Lworld.shape
+
+    gray_img = np.log(Lworld)
+
+    d = np.clip(math.ceil(0.01 * min(H, W)), 5, 11)
+    low = cv2.bilateralFilter(gray_img, d, sigma_range, 0.02 * max(H, W))
+    high = gray_img - low
+
+    factor = contrast / (np.max(gray_img) - np.min(gray_img))
+    log_abs_scale = np.max(gray_img) * factor
+    # print(factor, log_abs_scale)
+
+    new_gray = low * factor + high - log_abs_scale
+
+    Ld = np.exp(new_gray)
+
+    if save_filtered:
+        low = np.exp(low)
+        filename = f"bilateral_low_{sigma_range}_{contrast}_{a}_{Lwhite}_{delta}_{normalize}.png"
+        filename = os.path.join(output_dir, filename)
+        print(f"saving low frequency image to {filename}")
+        cv2.imwrite(filename, single_global(low, a, Lwhite, delta) * 255)
+
+        high = np.exp(high)
+        filename = f"bilateral_high_{sigma_range}_{contrast}_{a}_{Lwhite}_{delta}_{normalize}.png"
+        filename = os.path.join(output_dir, filename)
+        print(f"saving high frequency image to {filename}")
+        cv2.imwrite(filename, single_global(high, a, Lwhite, delta) * 255)
+
+        filename = f"bilateral_gray_{sigma_range}_{contrast}_{a}_{Lwhite}_{delta}_{normalize}.png"
+        filename = os.path.join(output_dir, filename)
+        print(f"saving LDR gray image to {filename}")
+        cv2.imwrite(filename, single_global(Ld, a, Lwhite, delta) * 255)
+
+    ldr = np.zeros_like(hdr, dtype=np.uint8)
+    for i in range(3):
+        # ldr[:,:,i] = hdr[:,:,i] * Ld / Lworld * 255
+        ldr[:,:,i] = single_global(hdr[:,:,i] * Ld / Lworld, a, Lwhite, delta) * 255
+        if normalize == NormalizeType.CHANNEL:
+            ldr[:,:,i] = cv2.normalize(ldr[:,:,i], None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    if normalize == NormalizeType.ALL:
+        ldr = cv2.normalize(ldr, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    filename = f"bilateral_{sigma_range}_{contrast}_{a}_{Lwhite}_{delta}_{normalize}.png"
+    filename = os.path.join(output_dir, filename)
+    print(f"saving LDR image to {filename}")
+    cv2.imwrite(filename, ldr)
+
+    return ldr
+
+def main(input_file:str, output_dir:str):
+    hdr, arg_list = utils.read_tonemap_settings(input_file)
+    for algorithm, *args in arg_list:
+        print("")
+        if algorithm == 'cv2_drago':
+            gamma, saturation, bias, brightness = args
+            cv2_drago(hdr, output_dir, gamma, saturation, bias, brightness)
+        elif algorithm == 'gamma_intensity':
+            gamma, brightness, normalize = args
+            gamma_intensity(hdr, output_dir, gamma, brightness, normalize)
+        elif algorithm == 'gamma_color':
+            gamma, brightness, normalize = args
+            gamma_color(hdr, output_dir, gamma, brightness, normalize)
+        elif algorithm == 'global':
+            a, Lwhite, delta, normalize, save_gray = args
+            photographic_global(hdr, output_dir, a, Lwhite, delta, normalize, save_gray)
+        elif algorithm == 'bilateral':
+            sigma_range, contrast, a, Lwhite, delta, normalize, save_filtered = args
+            bilateral_filtering(hdr, output_dir, sigma_range, contrast, a, Lwhite, delta, normalize, save_filtered)
+        else:
+            print(f"Error: Algorithm name {algorithm} not found")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Read HDR image & arguments from information in <input_file> & output the LDR images to <output_directory>\n")
+    parser.add_argument("input_file", type=str, help="Input file (.txt) path")
+    parser.add_argument("output_directory", type=str, help="Output directory path")
+
+    # usage = parser.format_usage()
+    usage = "tonemap.py [-h] <input_file> <output_directory>\nExample: python tonemap.py img/test3/tonemap.txt img/test3\n"
+    parser.usage = usage
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.output_directory):
+        print(f"error: {args.output_directory} is not a directory\n")
+        parser.print_help()
+        exit()
+
+    main(args.input_file, args.output_directory)
---
+++ code/utils.py
@@ -0,0 +1,244 @@
+import os
+import cv2
+import numpy as np
+from align import AlignType, ThresholdType
+from tonemap import NormalizeType
+
+def perror(message:str):
+    print(message)
+    exit()
+
+def to_bool(value:str, file:str, line:int):
+    if value.capitalize() == 'True' or value == '1':
+        return True
+    elif value.capitalize() == 'False' or value == '0':
+        return False
+    else:
+        perror(f"Error in {file}, line {line+1}: {value} is not a valid bool value")
+
+def to_int(value:str, file:str, line:int):
+    try:
+        return int(value)
+    except ValueError:
+        perror(f"Error in {file}, line {line+1}: {value} is not a valid int value")
+
+def to_float(value:str, file:str, line:int):
+    try:
+        return float(value)
+    except ValueError:
+        perror(f"Error in {file}, line {line+1}: {value} is not a valid float value")
+
+def to_AlignType(value:str, file:str, line:int):
+    try:
+        return AlignType[value]
+    except KeyError:
+        perror(f"Error in {file}, line {line+1}: {value} is not a valid AlignType value")
+
+def to_ThresholdType(value:str, file:str, line:int):
+    try:
+        return ThresholdType[value]
+    except KeyError:
+        perror(f"Error in {file}, line {line+1}: {value} is not a valid ThresholdType value")
+
+def to_NormalizeType(value:str, file:str, line:int):
+    try:
+        return NormalizeType[value]
+    except KeyError:
+        perror(f"Error in {file}, line {line+1}: {value} is not a valid NormalizeType value")
+
+def read_ldr_images(image_list:str) -> tuple[list[cv2.Mat | np.ndarray[np.uint8, 3]], int, np.ndarray[np.float32], AlignType, int, int, ThresholdType, int]:
+    """
+    Read the image_list file (.txt) and read all images included in the list. Then converts images into r,g,b channels and log of exposure times
+
+    Parameters:
+    image_list : the path of image_list.txt
+
+    Returns:
+    images[j] : the jth image
+    lnt[j]    : the log delta t or log shutter speed for image j
+    l         : the lambda (smoothness) used in solving response curve
+    alignType : the alignment method to use
+    std_ing_idx : the standard image's index in the list of images (for alignment)
+    depth     : the recursion depth of MTB algorithm
+    threshold_type : threshold type in MTB algorithm
+    gray_range : the range of intensities to ignore around threshold
+    """
+
+    images = []
+    exposure_times = []
+    l = 20
+    align_type = AlignType.NONE
+    std_img_idx = -1
+    depth = 3
+    threshold_type = ThresholdType.MEDIAN
+    gray_range = 4
+
+    input_dir = os.path.dirname(image_list)
+
+    try:
+        with open(image_list, 'r') as img_list:
+            for i, line in enumerate(img_list):
+                line = line.split('#')[0].strip() # remove the comments starts with '#'
+                if len(line) == 0:
+                    continue
+
+                if line.startswith('ALIGN'):
+                    line.replace(' ', '')
+                    t = line.split('=')[1]
+                    align_type = to_AlignType(t, image_list, i)
+
+                elif line.startswith('STD'):
+                    line.replace(' ', '')
+                    std_img_idx = to_int(line.split('=')[1], image_list, i)
+
+                elif line.startswith('DEPTH'):
+                    line.replace(' ', '')
+                    depth = to_int(line.split('=')[1], image_list, i)
+
+                elif line.startswith('THRESHOLD'):
+                    line.replace(' ', '')
+                    t = line.split('=')[1]
+                    threshold_type = to_ThresholdType(t, image_list, i)
+
+                elif line.startswith('GRAYRANGE'):
+                    line.replace(' ', '')
+                    gray_range = to_int(line.split('=')[1], image_list, i)
+
+                elif line.startswith('LAMBDA'):
+                    line.replace(' ', '')
+                    l = to_float(line.split('=')[1], image_list, i)
+
+                else:
+                    parts = line.split()
+                    if len(parts) >= 2:
+                        filename, shutter_speed, *_ = parts
+                        filepath = os.path.join(input_dir, filename)
+                        print(f"reading file {filepath}")
+                        img = cv2.imread(filepath, cv2.IMREAD_COLOR)
+                        if img is None:
+                            perror(f"Error: Can not read file {filepath}")
+                        images.append(img)
+                        exposure_times.append(shutter_speed)
+                    else:
+                        perror(f"Error in {image_list}, line {i+1}: Not enough arguments")
+
+        assert(len(images) == len(exposure_times))
+        lnt = np.log(np.array(exposure_times, dtype=np.float32))
+
+        return (images, lnt, l, align_type, std_img_idx, depth, threshold_type, gray_range)
+
+    except FileNotFoundError as e:
+        perror(f"FileNotFoundError: {e}")
+
+def ldr_to_channels(images:list[cv2.Mat | np.ndarray[np.uint8, 3]]) -> list[np.ndarray[np.uint8, 3]]:
+    channels = [None] * 3
+    # channel 0,1,2 = B,G,R
+    for i in range(3):
+        channels[i] = np.array([img[:, :, i] for img in images], dtype=np.uint8)
+    return channels
+
+def read_hdr_image(filepath:str) -> np.ndarray[np.float32, 3]:
+    """
+    Read the .hdr file and convert it into numpy array with channel order BGR
+
+    Parameters:
+    filepath : the path of the .hdr file
+
+    Returns:
+    hdr_image[x,y,i] : the HDR value (float32) of pixel location (x, y) in the ith channel (BGR)
+    """
+
+    print(f"reading {filepath}")
+    hdr_image = cv2.imread(filepath, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
+    if hdr_image is None:
+        perror(f"Error: Can not read file {filepath}")
+    print(f"size = {hdr_image.shape}")
+    return hdr_image
+
+def save_hdr_image(hdr_image:np.ndarray[np.float32, 3], filename:str) -> None:
+    """
+    Write the numpy array with channel order BGR into .hdr file 
+
+    Parameters:
+    filename : the name of the .hdr file to save
+    """
+
+    print(f"saving HDR image to {filename}")
+
+    if filename.endswith('.hdr'):
+        cv2.imwrite(filename, hdr_image)
+    else:
+        cv2.imwrite(f"{filename}.hdr", hdr_image)
+
+def read_tonemap_arguments(args:list, file:str, line:int) -> list:
+    if len(args) < 1:
+        perror(f"Error in {file}, line {line+1}: Not enough arguments")
+
+    algorithm = args[0]
+
+    if algorithm == 'cv2_drago':
+        if len(args) != 5:
+            perror(f"Error in {file}, line {line+1}: Need 5 arguments, but got {len(args)}")
+        gamma = to_float(args[1], file, line)
+        saturation = to_float(args[2], file, line)
+        bias = to_float(args[3], file, line)
+        brightness = to_float(args[4], file, line)
+        return [algorithm, gamma, saturation, bias, brightness]
+
+    elif algorithm == 'gamma_intensity' or algorithm == 'gamma_color':
+        if len(args) != 4:
+            perror(f"Error in {file}, line {line+1}: Need 4 arguments, but got {len(args)}")
+        gamma = to_float(args[1], file, line)
+        brightness = to_float(args[2], file, line)
+        normalize = to_NormalizeType(args[3], file, line)
+        return [algorithm, gamma, brightness, normalize]
+
+    elif algorithm == 'global':
+        if len(args) != 6:
+            perror(f"Error in {file}, line {line+1}: Need 6 arguments, but got {len(args)}")
+        a = to_float(args[1], file, line)
+        Lwhite = to_float(args[2], file, line)
+        delta = to_float(args[3], file, line)
+        normalize = to_NormalizeType(args[4], file, line)
+        save_gray = to_bool(args[5], file, line)
+        return [algorithm, a, Lwhite, delta, normalize, save_gray]
+
+    elif algorithm == 'bilateral':
+        if len(args) != 8:
+            perror(f"Error in {file}, line {line+1}: Need 8 arguments, but got {len(args)}")
+        sigma_range = to_float(args[1], file, line)
+        contrast = to_float(args[2], file, line)
+        a = to_float(args[3], file, line)
+        Lwhite = to_float(args[4], file, line)
+        delta = to_float(args[5], file, line)
+        normalize = to_NormalizeType(args[6], file, line)
+        save_filtered = to_bool(args[7], file, line)
+        return [algorithm, sigma_range, contrast, a, Lwhite, delta, normalize, save_filtered]
+
+    else:
+        perror(f"Error in {file}, line {line+1}: Algorithm name {algorithm} not found")
+
+def read_tonemap_settings(setting_file:str) -> tuple[np.ndarray[np.float32, 3],list]:
+    hdr_img = None
+    arg_list = []
+
+    input_dir = os.path.dirname(setting_file)
+
+    try:
+        with open(setting_file, 'r') as setting:
+            for i, line in enumerate(setting):
+                line = line.split('#')[0].strip() # remove the comments starts with '#'
+                if len(line) == 0:
+                    continue
+                if line.startswith('FILE'):
+                    line.replace(' ', '')
+                    hdr_filename = line.split('=')[1]
+                    hdr_img = read_hdr_image(os.path.join(input_dir, hdr_filename))
+                else:
+                    args = line.split()
+                    arg_list.append(read_tonemap_arguments(args, setting_file, i))
+
+        return (hdr_img, arg_list)
+
+    except FileNotFoundError as e:
+        perror(f"FileNotFoundError: {e}")
